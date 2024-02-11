61e2b96081ea163f8b1452212149bd7d107d1e4c
ShivsundarMallick <112780114+ShivsundarMallick@users.noreply.github.com>
2024-02-11 19:52:06 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = undefined
+
+console.log(typeof score);
+
+let valueinnumber = Number(score);
+console.log(typeof valueinnumber)
+console.log(valueinnumber);
+
+//conversion-----------------
+//"33" => 33
+//"33abc" => NaN(Not a Number)
+//true => 1; false => 0
+//null => 0
+//undefined => NaN
+
+let islogged = 1;
+
+let booleanislogged = Boolean(islogged);
+console.log(booleanislogged); 
+
+//1 => true ; 0 => false
+//"" => false
+// "shiv" => true
+
+let somenumber = 33;
+
+let stringNumber = String(somenumber);
+console.log(somenumber);
+console.log(typeof somenumber);
