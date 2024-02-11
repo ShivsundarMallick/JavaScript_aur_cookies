88f50cea16d3fa57fbf20b62b6d69369d1900994
ShivsundarMallick <112780114+ShivsundarMallick@users.noreply.github.com>
2024-02-11 07:59:29 +0000

    git commit

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 12345
+let accountemail ="shiv@gmail.com"
+var accountpassword = "1212121"
+accountcity = "pune" // with out declaring the keyword js accepts the variable but this is a bad way to use.
+let accountstate 
+
+// new 
+
+accountemail ="hcverma.com"
+accountpassword="2323232"
+accountcity ="bengaluru"
+
+/*
+prefer not to use the var 
+because of issue in block scope and function scope.
+*/ 
+
+console.log(accountId);
+console.table([accountId,accountemail,accountpassword,accountcity,accountstate])
