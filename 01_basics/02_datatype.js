41a7f7c31ee0ea6341ff9e5afd234938c0cc6cbc
ShivsundarMallick <112780114+ShivsundarMallick@users.noreply.github.com>
2024-02-11 19:23:47 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict" // treat all JS code as newer version
+
+// alert (3 + 3) // we use alerts when using browser and not in node.js
+
+console.log(3 + 3)
+
+let name = "shiv"
+let age = 18
+let isloggedIn  =false
+
+// number => 2 to power 53
+// bigint
+// string = ""
+//boolean => true/false
+//null => standalone value ,and null is a object itself
+//undefined =>
+// symbol => unique 
+
+//object
+
+console.log(typeof name) //output = string
+console.log(typeof null) // output = object
+console.log(typeof undefined) // output = undefined
